d21a5a2e3ea4afad89a85afa869fa813e46c0e7c
Gamal Anwar <gamal.anwar2020@gmail.com>
2018-04-04 08:50:43 +0300

    start

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+    <link rel="stylesheet" href="assets/main.css">
+</head>
+<body>
+
+<div class="messages"></div>
+<div class="turn"></div>
+<table>
+    <tr>
+        <td class="item1" data-num="1"></td>
+        <td class="item2" data-num="2"></td>
+        <td class="item3" data-num="3"></td>
+    </tr>
+    <tr>
+        <td class="item4"data-num="4"></td>
+        <td class="item5"data-num="5"></td>
+        <td class="item6"data-num="6"></td>
+    </tr>
+    <tr>
+        <td class="item7"data-num="7"></td>
+        <td class="item8"data-num="8"></td>
+        <td class="item9"data-num="9"></td>
+    </tr>
+</table>
+<br/><label>Auto Player <input type="checkbox" id="autoplay"></label>
+
+<br/><input class="reset" type="button" value="Reset"/>
+
+
+
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+<script src="assets/main.js"></script>
+
+</body>
+</html>
---
+++ src/Move.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Api;
+
+
+class Move implements MoveInterface
+{
+
+    public function makeMove($boardState, $playerUnit ='X'){
+
+    }
+
+}
